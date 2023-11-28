d7ad1ed9c58490c3cce1444533fd16686d070068
Alex Linden <alextl2004@gmail.com>
2023-11-28 13:01:42 -0800

    remove boilerplate start css for slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import RangeSlider from "./components/RangeSlider";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RangeSlider min={-15} max={-5} value={-7} step={1} />
+      <RangeSlider min={5} max={250} value={170} step={1} />
+      <RangeSlider min={10} max={20} value={15} step={2} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
